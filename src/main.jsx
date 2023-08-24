4b3fdf6ad3d5a6702d469ef1bf75d829c79679cf
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-08-24 23:56:51 +0700

    Finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Index from './routes'
+import Root, {
+  loader as rootLoader,
+  action as rootAction
+} from './routes/root'
+import Contact, {
+  loader as contactLoader,
+  action as contactAction
+} from './routes/contact'
+import EditContact, {
+  action as EditAction
+} from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
+import ErrorPage from './error-page'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: 'contacts/:id',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction
+          },
+          {
+            path: 'contacts/:id/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: EditAction
+          },
+          {
+            path: 'contacts/:id/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an errror.</div>
+          }
+        ]
+      }
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
